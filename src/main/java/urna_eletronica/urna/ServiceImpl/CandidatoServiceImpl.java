8078de2f38164a365db917e0c14226fd2d4f34f1
MatheusFelips <126251768+MatheusFelips@users.noreply.github.com>
2024-12-13 15:27:21 -0300

    Update CandidatoServiceImpl.java
    modificado candidatoService

--- src/main/java/urna_eletronica/urna/ServiceImpl/CandidatoServiceImpl.java
+++ src/main/java/urna_eletronica/urna/ServiceImpl/CandidatoServiceImpl.java
@@ -20,43 +20,7 @@ public class CandidatoServiceImpl implements CandidatoService {
   @Autowired private CandidatoRepository candidatoRepository;
   @Autowired private PartidoRepository partidoRepository;
 
-  private Candidato toEntity(CandidatoVo vo){
-    
-    Candidato newCandidato = new Candidato();
-    newCandidato.setCargo(vo.getCargoDisputado());
-    newCandidato.setCpf(vo.getCpf());
-    newCandidato.setEmail(vo.getEmail());
-    newCandidato.setNome(vo.getNome());
-    newCandidato.setPartido(new Partido(vo.getPartidoVo().getNome(), vo.getPartidoVo().getSigla(), vo.getPartidoVo().getId()));
-    newCandidato.setSenha(vo.getSenha());
-    newCandidato.setPleito(new Pleito(vo.getPleitoVo().getId()));
-    newCandidato.setCodigo(vo.getCodigo());
 
-    return newCandidato;
-  }
-
-  private void verificarPartido(Candidato candidato, Long id){
-    Optional<Partido> partido = partidoRepository.findById(id);
-    
-    if(!partido.isPresent()){
-       candidato.setPartido(partido.get());
-    }
-  }
-
-  @Override
-  public String cadastrarCandidato(CandidatoVo vo) {
-    Candidato candidato = toEntity(vo);
-    if(candidato != null){
-      try {
-        candidatoRepository.save(candidato);
-        return "Candidato cadastrado com sucesso";
-      } catch (Exception e) {
-        System.out.println(e.getMessage());
-        System.out.println("Erro a salvar o candidato");
-      }
-    }
-    return "Candidato não foi salvo";
-  }
 
   @Override
   public List<CandidatoVo> findAllCandidato() {
