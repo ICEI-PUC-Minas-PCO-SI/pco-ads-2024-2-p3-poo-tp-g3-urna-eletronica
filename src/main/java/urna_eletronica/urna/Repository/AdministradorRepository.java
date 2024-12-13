5984d000f248cc47d797a331750b6cda7587af2e
MatheusFelips <126251768+MatheusFelips@users.noreply.github.com>
2024-12-13 15:11:47 -0300

    Update AdministradorRepository.java
    Criado o respository administrador

--- src/main/java/urna_eletronica/urna/Repository/AdministradorRepository.java
+++ src/main/java/urna_eletronica/urna/Repository/AdministradorRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import urna_eletronica.urna.Entity.Administrador;
+
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
   
